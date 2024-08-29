651ed0fe50597711727c3d155f1a5e0be2f05bdb
TIVMOF <tomyivanov06@gmail.com>
2024-08-29 16:07:42 +0000

    fix: service now finding path

--- codbex-orders-widgets/services/average-sales-order-price.js
+++ codbex-orders-widgets/services/average-sales-order-price.js
@@ -6,6 +6,10 @@ const widgetData = {
     order: 7
 };
 
+export function getWidget() {
+    return widgetData;
+}
+
 if (typeof exports !== 'undefined') {
     exports.getWidget = function () {
         return widgetData;
